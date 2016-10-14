594deb7bbb60b96ef0dfecbc16b873bc836429ab
Kathleen P Chase <kpchase@alionscience.com>
2016-10-14 14:18:31 -0400

    fixed error in windows filename

--- cron_bkp_to_ext_HD.sh
+++ cron_bkp_to_ext_HD.sh
@@ -5,8 +5,8 @@ NOW=$(date +%F)
 DESDIR=/cygdrive/e
 FILENAME=cronbkp_$NOW.tar.gz
 
-SRCDIR1=/cygdrive/c/Users/kpchase/My\ Documents/acme
+SRCDIR1="/cygdrive/c/Users/kpchase/My Documents/acme"
 SRCDIR2=/cygdrive/c/acmedev
 
-tar -cpzf $DESDIR/$FILENAME $SRCDIR1 $SRCDIR2
+tar -cpzf $DESDIR/$FILENAME "$SRCDIR1" "$SRCDIR2"
 chown -R kpchase $DESDIR/$FILENAME
