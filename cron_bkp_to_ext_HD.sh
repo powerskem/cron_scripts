d9aa414b1e29371f88dc5aae072fcb0f6073464f
Kathleen P Chase <kpchase@alionscience.com>
2017-01-05 12:00:07 -0500

    added rsync of 2016archive and updated user dirs to tar.gz

--- cron_bkp_to_ext_HD.sh
+++ cron_bkp_to_ext_HD.sh
@@ -2,11 +2,16 @@
 
 NOW=$(date +%F)
 
-DESDIR=/cygdrive/e
-FILENAME=cronbkp_$NOW.tar.gz
-
-SRCDIR1="/cygdrive/c/Users/kpchase/My Documents/acme"
-SRCDIR2=/cygdrive/c/acmedev
+SRCDIR1="/cygdrive/c/Users/kpchase/"
+SRCDIR2="/cygdrive/c/cygwin64/home/kpchase/"
+DESDIR="/cygdrive/e/"
+FILENAME="cronbkp_$NOW.tar.gz"
 
 tar -cpzf $DESDIR/$FILENAME "$SRCDIR1" "$SRCDIR2"
 chown -R kpchase $DESDIR/$FILENAME
+
+
+SRCDIR3="/cygdrive/c/2016archive/"
+DESDIR3="/cygdrive/e/2016archive/"
+
+rsync -rhauvzP "$SRCDIR3" "$DESDIR3"
